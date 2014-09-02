257a87d2c4575f29d58e80135f610c13f99f9887
Amitay Horwitz <amitayh@il.ibm.com>
2014-09-02 16:10:49 +0300

    Fix in fixtures after escodegen update

--- specs/fixtures/compiled.js
+++ specs/fixtures/compiled.js
@@ -1,28 +1,28 @@
 (function () {
   // Source: ROOT/modD/sub2.js
   var sub2 = function () {
-      return function (str) {
-        return str + "?";
-      };
-    }();
+    return function (str) {
+      return str + "?";
+    };
+  }();
   // Source: ROOT/modD/sub1.js
   var sub1 = function () {
-      return function (str) {
-        return str + "!";
-      };
-    }();
+    return function (str) {
+      return str + "!";
+    };
+  }();
   // Source: ROOT/modC.js
   var modC = function (d1, d2) {
-      return d1("C") + d2("C");
-    }(sub1, sub2);
+    return d1("C") + d2("C");
+  }(sub1, sub2);
   // Source: ROOT/modB.js
   var modB = function (c, d2) {
-      return d2("B") + c;
-    }(modC, sub2);
+    return d2("B") + c;
+  }(modC, sub2);
   // Source: ROOT/modA.js
   var modA = function (c, d1) {
-      return d1("A") + c;
-    }(modC, sub1);
+    return d1("A") + c;
+  }(modC, sub1);
   // Source: ROOT/main.js
   (function (a, b) {
     console.log(a, b);
